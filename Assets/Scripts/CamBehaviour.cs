1517ad13d2714469652a014576950b475b96558b
MrRosso <miguelrossoyt@gmail.com>
2015-04-18 13:59:35 +0100

    Empezando Cámara

---
+++ Assets/Scripts/CamBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamBehaviour : MonoBehaviour {
+
+	public Transform camTransform;
+	public GameObject player;
+
+	public float x1;
+	public float y1;
+
+	// Use this for initialization
+	void Start () {
+		//if (player.GetComponent<AxelController>().
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
