1864cb63a462df594d8912a991a7e34f1ce29ecb
MrRosso <miguelrossoyt@gmail.com>
2015-04-18 15:33:03 +0100

    Cámara terminada (DDScene 1)

--- Assets/Scripts/CamBehaviour.cs
+++ Assets/Scripts/CamBehaviour.cs
@@ -6,16 +6,43 @@ public class CamBehaviour : MonoBehaviour {
 	public Transform camTransform;
 	public GameObject player;
 
-	public float x1;
-	public float y1;
+	public float CamObjX;
+	public float CamObjY;
+	
+	//Vector3 yy;
 
 	// Use this for initialization
 	void Start () {
-		//if (player.GetComponent<AxelController>().
+		CamObjX=player.GetComponent<Transform>().position.x;
+		CamObjY=player.GetComponent<Transform>().position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			CamObjX=player.GetComponent<Transform>().position.x+3;
+		}
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			CamObjX=player.GetComponent<Transform>().position.x-3;
+		}
+		if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+			CamObjY=player.GetComponent<Transform>().position.y-3;
+		}
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+			CamObjY=player.GetComponent<Transform>().position.y+3;
+		}
 
+		if (CamObjX>camTransform.position.x) {//R
+			camTransform.position=new Vector3(camTransform.position.x-((camTransform.position.x-CamObjX)*0.1f),camTransform.position.y,camTransform.position.z);
+		}
+		if (CamObjX<camTransform.position.x) {//L
+			camTransform.position=new Vector3(camTransform.position.x+((CamObjX-camTransform.position.x)*0.1f),camTransform.position.y,camTransform.position.z);
+		}
+		if (CamObjY<camTransform.position.y) {//U
+			camTransform.position=new Vector3(camTransform.position.x,camTransform.position.y-((camTransform.position.y-CamObjY)*0.1f),camTransform.position.z);
+		}
+		if (CamObjY>camTransform.position.y) {//D
+			camTransform.position=new Vector3(camTransform.position.x,camTransform.position.y+((CamObjY-camTransform.position.y)*0.1f),camTransform.position.z);
+		}
 	}
 }
