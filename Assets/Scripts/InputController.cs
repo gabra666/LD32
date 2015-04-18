1d50cb9799ce822555e7fd6f6e4e7f4850ef97dd
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-04-18 16:46:42 +0200

    Refactoring

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 public class InputController : MonoBehaviour {
-    CharacterController characterController;
+    MovementController movementController;
+    AttackController attackController;
 
 	// Use this for initialization
 	void Start () {
-        characterController = gameObject.GetComponent<CharacterController>();
+        movementController = gameObject.GetComponent<MovementController>();
+        attackController = gameObject.GetComponent<AttackController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        characterController.setMovementVector(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        movementController.setMovementVector(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
         if (Input.GetKeyDown(KeyCode.Space))
-            characterController.hit();
+            attackController.attack();
 	}
 }
