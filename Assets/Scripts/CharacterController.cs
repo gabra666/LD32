cb62e4ea0bb92467cf11934b579e5183150a7db9
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-04-18 14:13:30 +0200

    Movement character with animations

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    public float speed = 0.2f;
+    private bool facingRight = true;
+    private bool punching = false;
+    private Vector3 actualMovement;
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        actualMovement = new Vector3(0, 0, 0);
+        animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (punching)
+        {
+            punching = false;
+            animator.SetBool("punching", false);
+        }
+        gameObject.transform.position += actualMovement * speed * Time.deltaTime;
+        animator.SetFloat("HorizontalMovement", Mathf.Abs(actualMovement.x));
+        if ((facingRight && actualMovement.x < 0) || (!facingRight && actualMovement.x > 0))
+            flip();
+	}
+
+    private void flip()
+    {
+        facingRight = !facingRight;
+        Quaternion rotation = transform.rotation;
+        rotation.y = facingRight ? 0 : 180;
+        transform.rotation = rotation;
+    }
+
+    public void setMovementVector(Vector3 movement)
+    {
+        actualMovement = movement;
+    }
+
+    public void hit()
+    {
+        punching = true;
+        animator.SetBool("punching", true);
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class InputController : MonoBehaviour {
+    CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+        characterController = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        characterController.setMovementVector(new Vector3(Input.GetAxis("Horizontal"), 0, 0));
+        if (Input.GetKeyDown(KeyCode.Space))
+            characterController.hit();
+	}
+}
