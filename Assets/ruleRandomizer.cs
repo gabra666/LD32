1664ae1ec77eba40abc5483f0d0017f2af2c35b4
adrian_rivero <adrirp149@yahoo.es>
2015-04-19 20:46:11 +0100

    Main Scenes RUles Added

---
+++ Assets/ruleRandomizer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ruleRandomizer : MonoBehaviour {
+
+
+	public Sprite[] rules;
+	private float random;
+	// Use this for initialization
+	void Start () {
+
+		random = (int)Random.Range (0, 7);
+		this.GetComponent<Image> ().sprite = rules [(int)random];
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
