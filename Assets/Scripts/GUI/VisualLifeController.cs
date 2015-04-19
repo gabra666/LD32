ce1da1a2aa6be389c3e16365ef049287f75a1f30
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-04-19 10:28:27 +0200

    Life bars implemented

---
+++ Assets/Scripts/GUI/VisualLifeController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class VisualLifeController : MonoBehaviour
+{
+    public Life playerLife;
+
+    private float currentLife;
+    private float visualCurrentLife;
+
+    public RectTransform lifeTransform;
+    private float visualLifeMaxWidth;
+    private float visualLifeStartPosition;
+    private int maxLife;
+    public Image visualLife;
+    private bool isPlayer2;
+    public float maxTimeLifebarAnimation = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        maxLife = (int) playerLife.maximunLife;
+        currentLife = maxLife;
+        visualCurrentLife = currentLife;
+        visualLife.color = new Color(1 - ((float)visualCurrentLife / (float)maxLife), ((float)visualCurrentLife / (float)maxLife), 0f);
+        visualLifeMaxWidth = lifeTransform.sizeDelta.x;
+        visualLifeStartPosition = lifeTransform.localPosition.x;
+        isPlayer2 = playerLife.gameObject.tag == "Player2";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float damage = currentLife - playerLife.actualLife;
+        currentLife -= damage;
+        StartCoroutine(CoolDownDamage(damage));
+    }
+
+    void handleLife()
+    {
+        
+        lifeTransform.sizeDelta = new Vector2((visualLifeMaxWidth / maxLife) * visualCurrentLife, lifeTransform.sizeDelta.y);
+        float xDisplacement = (isPlayer2 ? 1 : -1) * ((visualLifeMaxWidth - lifeTransform.sizeDelta.x) / 2);
+        Vector2 newLocalPos = lifeTransform.localPosition = new Vector2(visualLifeStartPosition + xDisplacement, lifeTransform.localPosition.y);
+        visualLife.color = new Color(1 - ((float)visualCurrentLife / (float)maxLife), ((float)visualCurrentLife / (float)maxLife), 0f);
+    }
+
+    IEnumerator CoolDownDamage(float damage)
+    {
+        float timeCoolDown = maxTimeLifebarAnimation / damage;
+        for (int i = 0; i < damage; ++i)
+        {
+            --visualCurrentLife;
+            handleLife();
+            yield return new WaitForSeconds(timeCoolDown);
+        }
+
+    }
+}
