8e0bfbbbdd9a654f06df72ef52139b3d6704592b
Rayco Sánchez García <raycosanchezgarcia@gmail.com>
2015-04-19 14:20:46 +0200

    First version from menu finished

---
+++ Assets/CreditsController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsController : MonoBehaviour {
+
+    private CanvasGroup canvasGroup;
+
+    // Use this for initialization
+    void Start()
+    {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void BringToFront()
+    {
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1;
+    }
+}
