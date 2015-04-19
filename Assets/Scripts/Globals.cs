f6cde8a636b6fa0fa68cd9b5842ad4ba79d6a7f6
adrian_rivero <adrirp149@yahoo.es>
2015-04-19 10:29:07 +0100

    Main Menu Subido

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour {
+
+
+	public static bool seenSplash=true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
